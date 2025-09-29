45fa6abe95419028768f21825e1ac830de8c9dfe
ReLvLGo <Kurt056@mail.ru>
2025-09-29 22:04:05 +0400

    Добавьте файлы проекта.

---
+++ FirstApplication.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FirstApplication.ConsoleApp
+
+{
+    class Program
+    {
+        static void Main (string[] args)    
+        {
+            Console.WriteLine("Как вас зовут?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет?");
+            String Age = Console.ReadLine();
+            Console.WriteLine("Какое ваше любимое хобби?");
+            String Hobby = Console.ReadLine();
+            Console.WriteLine("Здравствуйте, {0}", name);
+            Console.WriteLine("Мне сообщили что вам {0} лет", Age);
+            Console.WriteLine("{1} - это очень полезное занятие, так держать {0}",name, Hobby);
+
+        }
+    }
+}
+
