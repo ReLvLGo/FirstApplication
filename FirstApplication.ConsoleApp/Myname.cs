247868d386c7655e16213e321dbc4fd6412f03f0
ReLvLGo <Kurt056@mail.ru>
2025-10-03 21:38:39 +0400

    1

---
+++ FirstApplication.ConsoleApp/Myname.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstApplication.ConsoleApp;
+
+class Myname
+{
+
+    static void Main (string[] args)
+    {
+        string Myname;
+        Myname = "Dmitry";
+        Console.WriteLine(Myname);
+        Console.ReadKey();
+
+    }
+}
