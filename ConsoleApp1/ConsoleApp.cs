65e0a66b99acd1075e16395951336dc0a1829d79
ReLvLGo <Kurt056@mail.ru>
2025-10-05 21:15:16 +0400

    ASLL #

--- ConsoleApp1/ConsoleApp.cs
+++ ConsoleApp1/ConsoleApp.cs
@@ -3,15 +3,6 @@ class ConsoleApp
 {
     public static void Main(string[] args)
     {
-
-        const string myName = "Дмитрий";
-
-        Console.WriteLine(myName);
-        Console.WriteLine("\t\nПривет");
-        Console.WriteLine("\n\t   Мне 35 лет");
-        Console.WriteLine("\t\nMy name is \n\n\t   Дмитрий");
-
-
-        Console.ReadKey();
+        Console.WriteLine("\u0023");
     }
 }
