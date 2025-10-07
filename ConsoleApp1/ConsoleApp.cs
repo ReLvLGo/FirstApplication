92c3949d729d779e3c6b35156522c78fe03383e6
ReLvLGo <Kurt056@mail.ru>
2025-10-07 21:30:04 +0400

    Типы данных

--- ConsoleApp1/ConsoleApp.cs
+++ ConsoleApp1/ConsoleApp.cs
@@ -3,6 +3,14 @@ class ConsoleApp
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("\u0023");
+        string MyName = "Дмитрий";
+        byte age = 35;
+        bool HaveIApet = true;
+        double MyShoeSize = 42.5;
+
+        Console.WriteLine("Меня зовут " + MyName);
+        Console.WriteLine("Мне " + age + " лет");
+        Console.WriteLine("Есть ли у меня питомец? " + HaveIApet);
+        Console.WriteLine("Мой размер ноги " + MyShoeSize);
     }
 }
--- FirstApplication.ConsoleApp/Program.cs
+++ FirstApplication.ConsoleApp/Program.cs
@@ -2,22 +2,24 @@
 
 
 namespace FirstApplication.ConsoleApp
-
 {
+
     class Program
     {
-        static void Main (string[] args)    
+        static void Main(string[] args)
         {
             Console.WriteLine("Как вас зовут?");
             String name = Console.ReadLine();
             Console.WriteLine("Сколько вам лет?");
-            String Age = Console.ReadLine();
+            Byte Age = Console.ReadLine();
             Console.WriteLine("Какое ваше любимое хобби?");
             String Hobby = Console.ReadLine();
             Console.WriteLine("Здравствуйте, {0}", name);
             Console.WriteLine("Мне сообщили что вам {0} лет", Age);
-            Console.WriteLine("{1} - это очень полезное занятие, так держать {0}",name, Hobby);
+            Console.WriteLine("{1} - это очень полезное занятие, так держать {0}", name, Hobby);
+       Console.ReadLine();
         }
+
     }
 }
 
